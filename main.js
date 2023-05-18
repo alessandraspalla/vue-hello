3506ec0e1557735805e6e5b07bfb979032d34007
alessandraspalla <alespalla93@icloud.com>
2023-05-18 17:45:27 +0200

    add image with data() property

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ const app = createApp({
   data() {
     return {
         message: 'Hello Vue!!',
-        nameClass: 'titolo'
+        nameClass: 'titolo',
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png"
     }
   }
 }).mount('#title');
